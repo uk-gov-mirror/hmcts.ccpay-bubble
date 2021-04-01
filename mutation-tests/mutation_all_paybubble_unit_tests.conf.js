86cde07129e1bc49a6e6b9e9ff19de276d82f1d1
Satish Ramachandrappa <satish.ramachandrappa@hmcts.net>
2021-04-01 10:20:50 +0100

    only adding minimal test

--- mutation-tests/mutation_all_paybubble_unit_tests.conf.js
+++ mutation-tests/mutation_all_paybubble_unit_tests.conf.js
@@ -22,8 +22,8 @@ module.exports = {
   ],
   mutate: [
     // Components
-    'src/app/components/**/*.ts',
-    '!src/app/components/**/*.spec.ts'
+    // 'src/app/components/**/*.ts',
+    // '!src/app/components/**/*.spec.ts',
     // //  Models
     // 'src/app/models/**/*.ts',
     // '!src/app/models/**/*.spec.ts',
@@ -36,12 +36,12 @@ module.exports = {
     // '!src/app/services/**/*.spec.ts',
     // '!src/app/services/caseref/caseref.service.ts',
     // //  Shared
-    // 'src/app/shared/**/*.ts',
-    // '!src/app/shared/**/*.spec.ts',
-    // '!src/app/shared/interceptors/auth.dev.interceptor.ts',
-    // '!src/app/shared/validators/help-with-fees.validator.ts',
-    // '!src/app/shared/validators/index.ts',
-    // '!src/app/shared/components/header/header.component.ts'
+    'src/app/shared/**/*.ts',
+    '!src/app/shared/**/*.spec.ts',
+    '!src/app/shared/interceptors/auth.dev.interceptor.ts',
+    '!src/app/shared/validators/help-with-fees.validator.ts',
+    '!src/app/shared/validators/index.ts',
+    '!src/app/shared/components/header/header.component.ts'
 
 
   ],
